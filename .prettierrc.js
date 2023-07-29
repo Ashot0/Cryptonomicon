5c938e77c098cb0fcd4902ab5a6baacd11080e4d
Ashot0 <illia.holovan@gmail.com>
2023-07-29 21:23:18 +0300

    TailwindShort

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  useTabs: true,
-  //   trailingComma: false,
-  //   semi: true, // использовать точку с запятой ('true')
-  //   singleQuote: true, // использовать одинарные кавычки ('false')
-  //   printWidth: 120, // длина переноса строки (80)
-  //   singleAttributePerLine: true, // один атрибут на строку ('false')
+	useTabs: true,
+	//   trailingComma: false,
+	//   semi: true, // использовать точку с запятой ('true')
+	//   singleQuote: true, // использовать одинарные кавычки ('false')
+	//   printWidth: 120, // длина переноса строки (80)
+	//   singleAttributePerLine: true, // один атрибут на строку ('false')
 };
--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
 								class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
 								placeholder="Например DOGE" />
 						</div>
-						<div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+						<div class="flex bg-white shadow-md p-1 rounded-md flex-wrap">
 							<span
 							@click="ticker= 'BTC', add()"
 								class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
